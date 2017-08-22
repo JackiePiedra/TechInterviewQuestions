13ecf9218700e937959f37a8c8951ae465af974c
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-22 18:06:21 -0400

    Fizzbuzz question

--- TechInterviewQuestions/Program.cs
+++ TechInterviewQuestions/Program.cs
@@ -10,6 +10,25 @@ namespace TechInterviewQuestions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter an integer:");
+            int integer = int.Parse(Console.ReadLine());
+
+            if(integer % 15 == 0)
+            {
+                Console.WriteLine("FIZZBUZZ");
+            }
+            else if(integer % 3 == 0)
+            {
+                Console.WriteLine("FIZZ");
+            }
+            else if(integer % 5 == 0)
+            {
+                Console.WriteLine("BUZZ");
+            }
+            else
+            {
+                Console.WriteLine("This number is not divisible by 3 or 5.");
+            }
         }
     }
 }
