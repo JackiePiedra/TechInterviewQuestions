99f3daf1da490ff2c61e05632a0182f446c72b88
JackiePiedra <jackie.r.piedra@gmail.com>
2017-09-12 19:47:59 -0400

    print prime numbers from 1 to 100

--- TechInterviewQuestions/Program.cs
+++ TechInterviewQuestions/Program.cs
@@ -16,15 +16,15 @@ namespace TechInterviewQuestions
             Console.WriteLine("Please enter an integer:");
             int integer = int.Parse(Console.ReadLine());
 
-            if(integer % 15 == 0) //15 takes care of both 3 and 5 but can write each mod separately with &&
+            if (integer % 15 == 0) //15 takes care of both 3 and 5 but can write each mod separately with &&
             {
                 Console.WriteLine("FIZZBUZZ");
             }
-            else if(integer % 3 == 0)
+            else if (integer % 3 == 0)
             {
                 Console.WriteLine("FIZZ");
             }
-            else if(integer % 5 == 0)
+            else if (integer % 5 == 0)
             {
                 Console.WriteLine("BUZZ");
             }
@@ -32,6 +32,47 @@ namespace TechInterviewQuestions
             {
                 Console.WriteLine("This number is not divisible by 3 or 5.");
             }
+
+            //print out all prime numbers than are less than 100 to console
+            //List<int> totalNums = new List<int>();
+            List<int> primeNums = new List<int>() { 2, 3, 5, 7 };
+            int maxNum = 100;
+            for (int i = 2; i <= maxNum; i++)
+            {
+                primeNums.Add(i);
+            }
+
+            for (int i = 2; i <= maxNum; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                {
+                    primeNums.Remove(i);
+                    //primeNums.Add(i);
+                }
+            }
+
+            foreach (int num in primeNums)
+            {
+                Console.WriteLine(num);
+            }
+
+            bool isPrime = true;
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if(isPrime)
+            {
+                Console.WriteLine(i +" ");
+            }
+            isPrime = true;
         }
     }
 }
